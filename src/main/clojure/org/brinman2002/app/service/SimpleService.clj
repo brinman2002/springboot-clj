bfd357aed606fd1a161246165fc39e92c25f189e
Brandon Inman <brandonathome@outlook.com>
2015-07-26 23:26:30 -0500

    inject PlayerRepository into an atom; needs to be properly thread-local

--- src/main/clojure/org/brinman2002/app/service/SimpleService.clj
+++ src/main/clojure/org/brinman2002/app/service/SimpleService.clj
@@ -4,10 +4,19 @@
 (gen-class
   :name ^{RestController {} 
           RequestMapping {:value ["simple"]} } org.brinman2002.app.service.SimpleService 
-  :methods [[^{RequestMapping {:method [RequestMethod/GET]}} greet [] java.lang.String ]])
+  :methods [[^{RequestMapping {:method [RequestMethod/GET]}} greet [] java.lang.String ]
+            [^{org.springframework.beans.factory.annotation.Autowired {}} setPlayerRepository [org.brinman2002.app.service.PlayerRepository] void]])
+
+
+
+;; TODO move this to a proper thread-local construct; probably var
+(def repository (atom nil))
 
 (defn -greet
   [ x]
-  "This is a greeting")
+  (str "This is a greeting " @repository))
 
+(defn -setPlayerRepository
+  [_ repo]
+  (swap! repository (fn [_] repo)))
 
